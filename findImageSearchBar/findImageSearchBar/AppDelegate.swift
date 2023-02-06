3dc09eeb9344fb65e837030df6c69becc4b17155
DaniilSmirn0v <d1n.sm1rnv@gmail.com>
2023-02-06 14:36:08 +0300

    feat: Prepare project to work.

---
+++ findImageSearchBar/findImageSearchBar/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  findImageSearchBar
+//
+//  Created by Даниил Смирнов on 06.02.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let vc = ViewController()
+        window?.rootViewController = vc 
+        window?.overrideUserInterfaceStyle = .light
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
+
---
+++ findImageSearchBar/findImageSearchBar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  findImageSearchBar
+//
+//  Created by Даниил Смирнов on 06.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
