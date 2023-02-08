e958a85139c2f0e600b64e31aa64ce33946e4df9
DaniilSmirn0v <d1n.sm1rnv@gmail.com>
2023-02-08 10:32:43 +0300

    fix: updt api key.

--- findImageSearchBar/findImageSearchBar/Sources/Services/NetworkService/NetworkComponents/DefaultComponents.swift
+++ findImageSearchBar/findImageSearchBar/Sources/Services/NetworkService/NetworkComponents/DefaultComponents.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 struct DefaultComponents {
-    static let apiKey = "3f50a6866edda270e7cd3e87f601955cdcd42885fcaad947249fed4e2465d5e9"
+    static let apiKey = "c955a0293e0fa4f86972ab2d3048236226cc81be18f7e46951e0c9df9a155a37"
     static let baseUrl = "https://serpapi.com/"
     static let headlinersPath = "search.json/"
     static let searchType = "isch"
